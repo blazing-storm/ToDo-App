0bb4452f6d8c983b11c30822bb3033d2004f7b4d
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-02-10 02:23:39 +0530

    Completed the add task functionality and started with the rendering of tasks from db

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,25 +2,30 @@
 const Task = require('../models/task');
 
 module.exports.home = function(req, res) {
-    return res.render('home', {
-        title: "ToDo App"
-    });
+    Task.find({}, function(err, tasks) {
+        if(err) {
+            console.log(`Error in fetching the tasks from db: ${err}`);
+            return;
+        }
+        return res.render('home', {
+            title: "ToDo App",
+            task_list: tasks
+        });
+    })
 }
 
 // adding a new task to the database
 module.exports.addTask = function(req, res) {
-    console.log(req.body);
-    return res.redirect('back');
-    // Task.create({
-    //     description: req.body.description,
-    //     category: req.body.category,
-    //     dueDate: req.body.dueDate
-    // }, function(err, newTask) {
-    //     if(err) {
-    //         console.log(`Error in adding a new task: ${err}`);
-    //         return;
-    //     }
-    //     console.log(newTask);
-    //     return res.redirect('back');
-    // })
+    Task.create({
+        description: req.body.description,
+        category: req.body.category,
+        dueDate: req.body.dueDate
+    }, function(err, newTask) {
+        if(err) {
+            console.log(`Error in adding a new task: ${err}`);
+            return;
+        }
+        console.log(newTask);
+        return res.redirect('back');
+    })
 }
